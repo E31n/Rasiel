b30ef76f671513e1b0e2175578c8737cd2fcec47
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-05-30 23:17:45 +0530

    Deployment Attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 
-const isProd = process.env.NODE_ENV === 'production';
-console.log(`Running in ${isProd ? 'production' : 'development'} mode`);
-
 // https://astro.build/config
 export default defineConfig({
     integrations: [svelte()],
     vite: {
         plugins: [tailwindcss()],
     },
-    site: isProd
-        ? 'https://E13n.github.io/Raziel/'
-        : 'http://localhost:4321',
-    base: isProd ? '/Raziel/' : '/',
-
+    site: 'https://e13n.github.io',
+    base: '/Rasiel/',
+    trailingSlash: 'never',
+    output: 'static',
 });
